992747163b71b4a99385041993702b0cd255149d
TANS123 <44278831+TANS123@users.noreply.github.com>
2019-03-14 16:40:22 -0700

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,11 @@
+$("button").click(function(){
+    var name = $(".input").val();
+    $(".name").append(name);
+    var name = $(".married").val();
+      $(".span").append(name);
+          var name = $(".weddding").val();
+      $(".dog").append(name);
+       var name = $(".kids").val();
+      $(".cat").append(name);
+});
+ 
